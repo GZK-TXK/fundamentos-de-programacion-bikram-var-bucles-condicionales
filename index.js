48b4a4be377be392e1de87a00d348d1fad19095b
GZK-TXK <chocarrovaz.gaizka@gmail.com>
2026-04-07 10:14:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ let variableSinvalor;
 let booleano1;
 let boolano2;
 
-const
+const PI = 3.14;
+const TAU = 2 * PI;
+
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano2;
+let booleanoMix0,
+  booleanoMix0 = 
+    booleano1 || (booleano2 && (booleano1 || (booleano1 && !booleano2)));
+
+let incrementarDesp;
+incrementarDesp = 2;
+
